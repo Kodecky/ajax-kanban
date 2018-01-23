3ada25fb089dbd28ecd631bd37223d39be15a996
Damian Pawela <paweladamian@gmail.com>
2018-01-23 20:48:36 +0100

    Finish kanban board

---
+++ js/App.js
@@ -0,0 +1,32 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+    'X-Client-Id': '2756',
+    'X-Auth-Token': '6855f5063aa76d04ae23fa48ab030887'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function (response) {
+        setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+        var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+        col.createCard(card);
+    });
+}
